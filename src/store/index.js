1574d04c53d29263b5d865ce698502facf2ecaa1
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-10-15 02:34:16 +0300

    Redux Toolkit : using ConfigureStore()

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@
 
 import {createStore} from "redux";
 import Actions from "./actions.js";
-import {createSlice} from "@reduxjs/toolkit";
+import {createSlice, configureStore} from "@reduxjs/toolkit";
 import {decrement, increase, increment, toggle} from "./reducers.js";
 
 const initialState = {
@@ -23,7 +23,15 @@ const counterSlice = createSlice({
 })
 
 
-
-const store = createStore(counterSlice.reducer);
+//! configureStore(): provides merging multiple reducers for multiple slices
+//! it takes a configuration object as a parameter
+//! { reducer: }  Redux still need one main reducer function which is responsible for the global state
+//! the configureStore() will merge those multiple reducers into one global reducer
+const store = configureStore({
+    //! reducer: counterSlice.reducer,   ✅✅ you can use single reducer function
+    reducer: {  //! you can use a map of reducers ✅✅
+        counter : counterSlice.reducer
+    }
+});
 
 export default store;
