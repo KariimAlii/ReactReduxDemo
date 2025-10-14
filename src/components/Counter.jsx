f8f3e99dc1f6cfc79e745408fe9169ea6704fc87
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-10-15 01:37:53 +0300

    Subscribe Component to the Redux Store

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,12 +1,17 @@
 import classes from './Counter.module.css';
+import {useSelector} from "react-redux";
 
 const Counter = () => {
+    //! Redux will set a subscription to redux store from this component
+    //! Counter Component <---------Subscribe-----------> Store
+    //! Whenever the value (state.counter) is updated in store => it will send the new value to the component
+    const counter = useSelector(state => state.counter)
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{ counter }</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
