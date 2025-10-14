44c549c6c43db826adcfe36ed43036f6ed1b67e4
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-10-15 02:23:46 +0300

    Redux Toolkit : Create Slice

--- src/store/index.js
+++ src/store/index.js
@@ -2,17 +2,32 @@
 
 import {createStore} from "redux";
 import Actions from "./actions.js";
+import {createSlice} from "@reduxjs/toolkit";
+import {decrement, increase, increment, toggle} from "./reducers.js";
 
 const initialState = {
     counter : 0,
     showCounter: true
 }
+
+//! We are creating of our global state (counter-related state)
+createSlice({
+    name: 'COUNTER',
+    initialState,
+    reducers: {
+        increment,
+        decrement,
+        increase,
+        toggle
+    }
+})
+
 //! Reducer is a pure function that updates the store
 //! but it provides immutability of state =>  always returns a totally new state object
 const counterReducer = (state = initialState, action) => {
     switch(action.type) {
         case Actions.INCREMENT:
-            return {
+            return {   //! State Immutability
                 ...state,
                 counter: state.counter + 1
             }
---
+++ src/store/reducers.js
@@ -0,0 +1,16 @@
+﻿export const increment = (state) => ({
+    ...state,
+    counter: state.counter + 1
+})
+export const decrement = (state) => ({
+    ...state,
+    counter: state.counter - 1
+})
+export const increase = (state, payload) => ({
+    ...state,
+    counter: state.counter + payload.value
+})
+export const toggle = (state) => ({
+    ...state,
+    showCounter: !state.showCounter
+})
