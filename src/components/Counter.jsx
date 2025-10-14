d2e5d0986ceab441f7b77424c789629b1edb463a
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-10-15 01:41:32 +0300

    Dispatch Actions from inside components

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,17 +1,33 @@
 import classes from './Counter.module.css';
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 
 const Counter = () => {
+    const dispatch = useDispatch();
     //! Redux will set a subscription to redux store from this component
     //! Counter Component <---------Subscribe-----------> Store
     //! Whenever the value (state.counter) is updated in store => it will send the new value to the component
     const counter = useSelector(state => state.counter)
+
+    const incrementHandler = () => {
+        dispatch( {
+            type: 'increment'
+        } )
+    }
+    const decrementHandler = () => {
+        dispatch( {
+            type: 'decrement'
+        } )
+    }
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{ counter }</div>
+        <div>
+            <button onClick={incrementHandler}>Increment</button>
+            <button onClick={decrementHandler}>Decrement</button>
+        </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -5,12 +5,14 @@ import {createStore} from "redux";
 const counterReducer = (state = { counter : 0 }, action) => {
     if(action.type === 'increment') {
         return {
+            ...state,
             counter: state.counter + 1
         }
     }
 
     if(action.type === 'decrement') {
         return {
+            ...state,
             counter: state.counter - 1
         }
     }
