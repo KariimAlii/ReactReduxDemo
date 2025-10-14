9cd470be09b97e5378f73d9a61a827bef3b33ff6
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-10-15 02:27:23 +0300

    Redux Toolkit : Internally manages Immutable State

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,16 +1,11 @@
-﻿export const increment = (state) => ({
-    ...state,
-    counter: state.counter + 1
-})
-export const decrement = (state) => ({
-    ...state,
-    counter: state.counter - 1
-})
-export const increase = (state, payload) => ({
-    ...state,
-    counter: state.counter + payload.value
-})
-export const toggle = (state) => ({
-    ...state,
-    showCounter: !state.showCounter
-})
+﻿//! Redux Toolkit internally manages this mutation and translate it to creating a new object
+//! with only the updates you specify
+//! So It will provide State Immutability for you
+export const increment = (state) => state.counter++;
+export const decrement = (state) => state.counter--;
+export const increase = (state, action) => {
+    state.counter = state.counter + action.payload.value
+}
+export const toggle = (state) => {
+    state.showCounter = !state.showCounter
+}
