61b38b47528f95a535e04e2bfc3274fe4992df9c
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-10-15 01:47:12 +0300

    Code Refactoring

--- src/store/index.js
+++ src/store/index.js
@@ -3,28 +3,25 @@
 import {createStore} from "redux";
 
 const counterReducer = (state = { counter : 0 }, action) => {
-    if(action.type === 'increment') {
-        return {
-            ...state,
-            counter: state.counter + 1
-        }
+    switch(action.type) {
+        case 'increment':
+            return {
+                ...state,
+                counter: state.counter + 1
+            }
+        case 'decrement':
+            return {
+                ...state,
+                counter: state.counter - 1
+            }
+        case 'reset':
+            return {
+                ...state,
+                counter: state.counter + action.payload.value
+            }
+        default:
+            return state;
     }
-
-    if(action.type === 'decrement') {
-        return {
-            ...state,
-            counter: state.counter - 1
-        }
-    }
-
-    if(action.type === 'increase') {
-        return {
-            ...state,
-            counter: state.counter + action.payload.value
-        }
-    }
-
-    return state;
 }
 
 const store = createStore(counterReducer);
